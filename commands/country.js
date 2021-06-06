637c2a5c0938fe17d9da0eb71af0bed9d2300ee5
SuhasDissa <suhasdissa@gmail.com>
2021-06-06 20:27:59 +0530

    fix bugs

--- commands/country.js
+++ commands/country.js
@@ -3,15 +3,16 @@ module.exports = {
 	name: 'country',
 	description: 'Ping!',
 	async execute(chatId, args,bot) {
+		args.shift();
 		
-		if(!args[0]) return msg.channel.send("Please tell me a country code");
-  var url = "https://restcountries.eu/rest/v2/name/" + args.join("%20");
+		if(!args[0]) bot.sendMessage(chatId,"Please tell me a country code");
+		var url = "https://restcountries.eu/rest/v2/name/" + args.join("%20");
   
 
   request(url, function(err, response, body) {
     let data = JSON.parse(body);
     
-    if(!data[0]) return msg.channel.send("Error");
+    if(!data[0]) bot.sendMessage(chatId,"Error");
 	
 	try {
             var messsage = `Country code: ${data[0].alpha2Code}\nCapital: ${data[0].capital}\nRegion: ${data[0].region}\nPopulation: ${data[0].population}\nArea: ${data[0].area} Sq km\nTimezone: ${data[0].timezones[0]}\nCurrency: ${data[0].currencies[0].name}\nCurrency Code: ${data[0].currencies[0].code}\nIDD Code: +${data[0].callingCodes[0]}`
