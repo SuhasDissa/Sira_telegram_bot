e441ad295fd43e75ed6e8caf4c0250bcca0d56ef
SuhasDissa <suhasdissa@gmail.com>
2021-06-07 06:37:27 +0530

    fixes

--- commands/country.js
+++ commands/country.js
@@ -10,11 +10,10 @@ module.exports = {
   
 
   request(url, function(err, response, body) {
-    let data = JSON.parse(body);
+    try {
+		let data = JSON.parse(body);
     
     if(!data[0]) bot.sendMessage(msg.chat.id,"Error");
-	
-	try {
             var messsage = `Country code: ${data[0].alpha2Code}\nCapital: ${data[0].capital}\nRegion: ${data[0].region}\nPopulation: ${data[0].population}\nArea: ${data[0].area} Sq km\nTimezone: ${data[0].timezones[0]}\nCurrency: ${data[0].currencies[0].name}\nCurrency Code: ${data[0].currencies[0].code}\nIDD Code: +${data[0].callingCodes[0]}`
 			
 			bot.sendMessage(msg.chat.id,messsage);
