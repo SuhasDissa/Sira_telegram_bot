e8f6f96e4786a239828ccf1a77b133c7b4b752b4
SuhasDissa <suhasdissa@gmail.com>
2021-06-07 06:47:34 +0530

    bugs fixed

--- commands/about.js
+++ commands/about.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'about',
 	description: 'Ping!',
 	async execute(msg, args, bot) {
-		bot.sendMessage(msg.chat.id,'*මේ Bot ගැන *\n\nමේ Bot ගොඩක් ප්‍රයෝජනවත් Bot කෙනෙක්. /help command එක මගින් ඔබට මෙ බොට් සහය දක්වන commands ලබාගත හැකිය. \nඅපි අලුත් commands නිතරම අතුලත් කරන්න උත්සාහ දරනවා.\nඑමනිසා අලුත් තොරතුරු /about හරහා ලබාගන්න.\nThis bot is delveloped by Suhas Dissanayake suhasdissa@gmail.com\n\nSource code can be found in https://github.com/SuhasDissa/Sira_telegram_bot\n\n*අලුත් තොරතුරු :*New commands added. Check /help',{parse_mode : "Markdown"});
+		bot.sendMessage(msg.chat.id,'*මේ Bot ගැන *\n\nමේ Bot ගොඩක් ප්‍රයෝජනවත් Bot කෙනෙක්. /help command එක මගින් ඔබට මෙ බොට් සහය දක්වන commands ලබාගත හැකිය. \nඅපි අලුත් commands නිතරම අතුලත් කරන්න උත්සාහ දරනවා.\nඑමනිසා අලුත් තොරතුරු /about හරහා ලබාගන්න.\nThis bot is delveloped by Suhas Dissanayake suhasdissa@gmail.com\n\nSource code can be found in https://github.com/SuhasDissa\n\n*අලුත් තොරතුරු :*New commands added. Check /help',{parse_mode : "Markdown"});
 	},
 };
